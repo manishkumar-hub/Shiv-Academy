44a987497f4cf43da17954d74e72f93b7d332e6a
Manish kumar <manishkumarpandit12@gmail.com>
2022-01-16 16:49:29 +0530

    Added Fade effect

--- src/Components/Alumni.js
+++ src/Components/Alumni.js
@@ -1,4 +1,6 @@
-import React, { PureComponent } from 'react'
+import React, { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 import Alumnicard from './Alumnicard';
 import manish from './AlumniImages/manish.jpg'
 import ashish from './AlumniImages/ashishsharma.jpeg'
@@ -6,63 +8,43 @@ import vikashraj from './AlumniImages/vikashkumarraj.jpeg'
 import ashwini from './AlumniImages/ashwinisingh.jpeg'
 import SujeetSharma from './AlumniImages/SujeetSharma.jpeg'
 import RaushanSwaraj from './AlumniImages/RaushanSwaraj.jpeg'
-class Alumni extends React.Component {
-    render() {
-        return <div>
+// class Alumni extends React.Component {
+// render() {
+const Alumni = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000, once: true, offset: 0, easing: 'ease-in-sine' });
+    }, []);
+    return (
+        <div>
             <h1 className='heading' id='alumhead'>ALUMNI</h1>
             <div className="container">
-                <div className="row">
-                    <div className="col">
+                <div className="row" >
+                    <div className="col" data-aos="fade-right" >
                         <Alumnicard name={'Vikash Kumar Raj'} description={<> MBBS <br /> AIIMS Kalyani </>} yos={'2008-17'} linkedinurl={'a'} instaurl={"d"} mailurl={'d'} ImageFileName={vikashraj} />
                     </div>
-                    <div className="col">
+                    <div className="col" data-aos="fade-right">
                         <Alumnicard name={'Manish kumar pandit'} description={<> B-tech in CSE <br /> NIT Sikkim</>} yos={'2009-17'} linkedinurl={"https://www.linkedin.com/in/manish-kumar-pandit-092107214"} instaurl={"df"} mailurl={"manishkumarpandit12@gmail.com"} ImageFileName={manish} />
 
                     </div>
-                    <div className="col">
+                    <div className="col" data-aos="fade-right" >
                         <Alumnicard name={'Ashish Sharma'} description={<>B-tech in ECE <br /> MNNIT Allahabad  </>} yos={'2008-17'} linkedinurl={'a'} instaurl={"d"} mailurl={'d'} ImageFileName={ashish} />
                     </div>
 
                 </div>
                 <div className="row">
-                    <div className="col">
+                    <div className="col" data-aos="fade-right">
                         <Alumnicard name={'Ashwini Singh'} description={<>B-tech in CE<br /> NIT Meghalaya </>} yos={'2006-17'} linkedinurl={'a'} instaurl={"d"} mailurl={'d'} ImageFileName={ashwini} />
                     </div>
-                    <div className="col">
+                    <div className="col" data-aos="fade-right" >
                         <Alumnicard name={'Sujeet Kumar Sharma'} description={<>B-tech in CSE<br /> Lovely Proffesional University </>} yos={'2006-17'} linkedinurl={'https://www.linkedin.com/in/sujeet-kumar-sharma-1494231b7'} instaurl={"d"} mailurl={'sks9097692531@gmail.com'} ImageFileName={SujeetSharma} />
                     </div>
-                    <div className="col">
-                    <Alumnicard name={'Raushan Swaraj'} description={<>Delhi University </>} yos={'2006-17'} linkedinurl={''} instaurl={""} mailurl={''} ImageFileName={RaushanSwaraj} />
+                    <div className="col" data-aos="fade-right">
+                        <Alumnicard name={'Raushan Swaraj'} description={<>Delhi University </>} yos={'2006-17'} linkedinurl={''} instaurl={""} mailurl={''} ImageFileName={RaushanSwaraj} />
                     </div>
                 </div>
             </div>
-            {/* <table>
-                <tr>
-                    <td>
-                     
-                    </td>
-                    <td>
-                     
-                    </td>
-                    <td>
-                     
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                     
-                    </td>
-                    <td>
-                     
-                    </td>
-                    <td>
-                     
-                    </td>
-                </tr>
-            </table> */}
-            {/* <Alumnicard/> */}
-        </div>;
-    }
+
+        </div>);
 }
 
 export default Alumni;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,89 +2,87 @@ import React, { Component, useEffect } from 'react'
 import Carousel from './Carousel';
 import Events from './Events';
 import './homecss.css'
-import secretary from './secretary.jpeg' 
+import secretary from './secretary.jpeg'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import RaushanSwaraj from './AlumniImages/RaushanSwaraj.jpeg'
-class Homepage extends React.Component {
-    render() {
-        return <div>
-            <Carousel/>
-            <Events/>
+// class Homepage extends React.Component {
+// render() {
+const Homepage = () => {
+    useEffect(() => {
+        Aos.init({duration: 2000 , once: true , offset:0 ,easing : 'ease-in-sine' });
+    }, []);
+    return (
+        <div>
+            <Carousel />
+            <Events />
             <div className='aimbox'>
                 <h1 className='topic-header heading' >VISION & MISSION </h1>
                 <p className='vis-para' >
-                The main Vision and Mission of the Association is to “Giving Back” to Alma Mater. Our aim is to bring all alumnus into one single network and help them connect with one another and with the Institute to join hands with it to foster and enhance it's academic pursuit of  knowledge.
-                    </p> 
-                    <p className="vis-para">
+                    The main Vision and Mission of the Association is to “Giving Back” to Alma Mater. Our aim is to bring all alumnus into one single network and help them connect with one another and with the Institute to join hands with it to foster and enhance it's academic pursuit of  knowledge.
+                </p>
+                <p className="vis-para">
                     Association aims to link all school alumnus together to develop synergistic plans to help and support all  students, alumni and society as a whole to achieve its vision, and to enable our alumnus add value to the society
-                    </p>
+                </p>
                 <h1 className='topic-header heading'>ABOUT THE ASSOCIATION </h1>
-                <div className="president ">
+                <div className="president" data-aos="fade-right">
                     <h2 className='sub-header' >PRESIDENT </h2>
                     <img id='presidentImage' src={secretary} alt="" />
                     <p id="ptext" >
-                    <br/>
+                        <br />
                         <h4 className='post-text'>ROBIN  </h4>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
                     </p>
                 </div>
-                <div className="vice-president animate fadeInRight two ">
+                <div className="vice-president" data-aos ="fade-left">
                     <h2 className='sub-header'>VICE-PRESIDENT</h2>
                     <img id='vice-presidentImage' src={secretary} alt="" />
                     <p id="vptext" >
-                    <br/>
+                        <br />
                         <h4 className='post-text'>UTKARSH </h4>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
                     </p>
                 </div>
-                <div className="secretary animate fadeInLeft three">
+                <div className="secretary " data-aos ="fade-right">
                     <h2 className='sub-header'> SECRETARY </h2>
                     <img id='secretary-Image' src={secretary} alt="" />
                     <p id="vptext" >
-                        <br/>
+                        <br />
                         <h4 className='post-text'>UTKARSH </h4>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
                     </p>
                 </div>
-                <div className="secretary animate fadeInLeft three">
+                <div className="secretary " data-aos ="fade-left">
                     <h2 className='sub-header'> VICE-SECRETARY </h2>
                     <img id='secretary-Image' src={RaushanSwaraj} alt="" />
                     <p id="vptext" >
-                        <br/>
+                        <br />
                         <h4 className='post-text'>RAUSHAN SWARAJ</h4>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
                     </p>
                 </div>
-                <div className="secretary animate fadeInRight four">
+                <div className="secretary " data-aos ="fade-right">
                     <h2 className='sub-header'> TREASURER </h2>
                     <img id='secretary-Image' src={secretary} alt="" />
                     <p id="vptext" >
-                        <br/>
+                        <br />
                         <h4 className='post-text'>UTKARSH </h4>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
+                        <br />
                     </p>
                 </div>
             </div>
-            {
-                // $(function () {
-                //     $(window).trigger("scroll");
-                //     var positiontop= $(document).scrollTop();
-                //     console.log(positiontop);
-                // })
-            }
-        </div>;
-    }
+        </div>
+    );
 }
 
 export default Homepage;
